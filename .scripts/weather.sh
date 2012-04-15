4d2e80f7ba0ec4e21050662927134311ca9083a7
marcel <knochen_der_unsterbliche@yahoo.de>
2012-04-15 20:54:37 +0200

    Update from thinkpad (So 15. Apr 20:54:37 CEST 2012)

--- .scripts/weather.sh
+++ .scripts/weather.sh
@@ -1,17 +1,26 @@
-#!/bin/sh
-#AccuWeather (r) RSS weather tool for conky
-#
-#USAGE: weather.sh <locationcode>
-#
-#(c) Michael Seiler 2007
-
+# !/usr/bin/bash
 METRIC=1 #Should be 0 or 1; 0 for F, 1 for C
-
 if [ -z $1 ]; then
-    echo
     echo "USAGE: weather.sh <locationcode>"
-    echo
     exit 0;
 fi
 
-curl -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${METRIC}\&locCode\=$1 | perl -ne 'if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; print "$1"; }'
+wget -q http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${METRIC}\&locCode\=$1 -O - | awk \
+'/Currently:/ {CurWeather=$0}
+/[0-9] Forecast<\/title>/ {nr=NR+5}
+NR==nr    {postIndx[++x]=$0}
+# x>2    {exit}
+END{
+    split(CurWeather,tmp,"Currently: ")
+    split(tmp[2],tmp1,"<")
+    CurWeather=sprintf("%s",tmp1[1])
+    sub(":",",",CurWeather)
+    for(x in postIndx){
+    split(postIndx[x],tmp,";|&")
+    split(tmp[1],tmp,">")
+    split(tmp[2],statement," C ") # substitute C for F if metric differs
+    split(tmp[2],temp)
+    forecast[++y]=sprintf("%d-%dC %-15s",temp[5],temp[2],statement[3])
+    }
+    printf("WEATHER:\nNow: %s\nToday: %s\nTomorrow: %s",CurWeather,forecast[1],forecast[2])
+}'
